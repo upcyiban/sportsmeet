be1020a67285241ea2d9f8f75ef738613416ba78
wanghaojun <549180928@qq.com>
2016-05-18 20:45:24 +0800

    建好啦competition类

--- src/main/java/cn/edu/upc/yb/sportsmeet/model/Competition.java
+++ src/main/java/cn/edu/upc/yb/sportsmeet/model/Competition.java
@@ -5,5 +5,48 @@ package cn.edu.upc.yb.sportsmeet.model;
  */
 public class Competition {
     private String name;
+    private int id;
+    private String[] studentnumber = new String[8];
+    private String[] studentname = new String[8];
+    private String[] studentmark = new String[8];
 
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String[] getStudentnumber() {
+        return studentnumber;
+    }
+
+    public String[] getStudentname() {
+        return studentname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStudentnumber(String[] studentnumber) {
+        this.studentnumber = studentnumber;
+    }
+
+    public void setStudentname(String[] studentname) {
+        this.studentname = studentname;
+    }
+
+    public void setStudentmark(String[] studentmark) {
+        this.studentmark = studentmark;
+    }
+
+    public String[] getStudentmark() {
+        return studentmark;
+    }
 }
