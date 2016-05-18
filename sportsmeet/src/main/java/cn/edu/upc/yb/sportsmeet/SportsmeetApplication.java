b9e0cb0db923a974a32d9bf3d73f66cca69c8766
yyljj <yyljj@outlook.com>
2016-05-18 19:47:43 +0800

    初始框架

---
+++ sportsmeet/src/main/java/cn/edu/upc/yb/sportsmeet/SportsmeetApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.upc.yb.sportsmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsmeetApplication.class, args);
+	}
+}
