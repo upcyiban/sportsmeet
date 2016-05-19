c6dc8adbb4081a47037011b93dbac7a1cf746d95
yyljj <yyljj@outlook.com>
2016-05-19 11:16:11 +0800

    增加inerthtml

--- src/main/java/cn/edu/upc/yb/sportsmeet/controller/InsertController.java
+++ src/main/java/cn/edu/upc/yb/sportsmeet/controller/InsertController.java
@@ -3,6 +3,7 @@ package cn.edu.upc.yb.sportsmeet.controller;
 import cn.edu.upc.yb.sportsmeet.model.Competition;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by 易班技术开发部 on 2016/5/18.
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class InsertController {
 
-    @RequestMapping("/insert")
-    public String InsertDate(String competition,String[] student,String[] studentname,String[] studentmark){
-        Competition people = new Competition(competition,student[8],studentname[8],studentmark[]);
+    @RequestMapping(name = "/insert",method = RequestMethod.POST)
+    public String InsertDate(String sportname, String studentname1, String studentgrate1, String studentname2, String studentgrate2, String studentname3, String studentgrate3, String studentname4, String studentgrate4, String studentname5, String studentgrate5, String studentname6, String studentgrate6, String studentname7, String studentgrate7, String studentname8, String studentgrate8){
+        Competition competition = new Competition(sportname,studentname1,studentgrate1,studentname2,studentgrate2,studentname3,studentgrate3,studentname4,studentgrate4,studentname5,studentgrate5,studentname6,studentgrate6,studentname7,studentgrate7,studentname8,studentgrate8);
+
+        return "insertsuccess";
 
 
     }
--- src/main/java/cn/edu/upc/yb/sportsmeet/model/Competition.java
+++ src/main/java/cn/edu/upc/yb/sportsmeet/model/Competition.java
@@ -1,13 +1,11 @@
 package cn.edu.upc.yb.sportsmeet.model;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 /**
  * Created by wanghaojun on 2016/5/18.
  */
+@Table(name = "Competition")
 @Entity
 public class Competition {
 
