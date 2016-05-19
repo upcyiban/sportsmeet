36b178a09b6d74139c66e957e4a2fd839793473e
yyljj <yyljj@outlook.com>
2016-05-19 12:39:54 +0800

    add insertamdin

--- src/main/java/cn/edu/upc/yb/sportsmeet/controller/InsertController.java
+++ src/main/java/cn/edu/upc/yb/sportsmeet/controller/InsertController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class InsertController {
 
+    @RequestMapping("/insertadmin")
+    public String showInertadin(){
+        return "insertadmin";
+    }
+
     @RequestMapping(name = "/insert",method = RequestMethod.POST)
     public String InsertDate(String sportname, String studentname1, String studentgrate1, String studentname2, String studentgrate2, String studentname3, String studentgrate3, String studentname4, String studentgrate4, String studentname5, String studentgrate5, String studentname6, String studentgrate6, String studentname7, String studentgrate7, String studentname8, String studentgrate8){
         Competition competition = new Competition(sportname,studentname1,studentgrate1,studentname2,studentgrate2,studentname3,studentgrate3,studentname4,studentgrate4,studentname5,studentgrate5,studentname6,studentgrate6,studentname7,studentgrate7,studentname8,studentgrate8);
